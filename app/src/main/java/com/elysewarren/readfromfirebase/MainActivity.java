7fbade080a5887d302633c44b606990fed3c167c
elysewarren <eawdesigns@aol.com>
2017-03-27 20:02:02 -0400

    Second push after Firebase

--- app/src/main/java/com/elysewarren/readfromfirebase/MainActivity.java
+++ app/src/main/java/com/elysewarren/readfromfirebase/MainActivity.java
@@ -1,13 +1,26 @@
 package com.elysewarren.readfromfirebase;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayText;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference personRef = database.getReference("person");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = (TextView) findViewById(R.id.display_text);
+
+        personRef.addValueEventListener(new ValueEventListener();
+
     }
-}
+    }
+
